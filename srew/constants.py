e902ccc5e2791540a5929a419fc7ef96c723cdac
Adept <BUT42@MAIL.RU>
2020-01-28 15:59:22 +0400

    file

--- srew/constants.py
+++ srew/constants.py
@@ -1,6 +1,6 @@
 # 30.12.2019
 def _r(value, name):
-	"""
+    """
     class _NamedIntConstant(int):
         def __new__(cls, n, v):
             self = super(_NamedIntConstant, cls).__new__(cls, v)
@@ -13,7 +13,7 @@ def _r(value, name):
         __repr__ = __str__
 
     return _NamedIntConstant(name, value)
-	"""
+    """
     return value
 
 
